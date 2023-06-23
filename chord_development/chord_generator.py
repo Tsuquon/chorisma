d45a5f73008389fae479ef4384cef04a2c747919
Tsuquon <liamling04@gmail.com>
2023-06-23 13:34:06 +1000

    added accidentals

--- chord_development/chord_generator.py
+++ chord_development/chord_generator.py
@@ -7,7 +7,11 @@ import fnmatch
 
 """
 This function generates random chords, creates an audiowave saves file
-
+system arguments can be given:
+    dest=<directory destination>; by default is stored-chords
+    -chord-develop: by default; choose to develop chords
+    -no-develop-chord: doesn't develop chord
+    -debug-mode: prints outputs
 """
 
 
@@ -18,16 +22,21 @@ class ChordGenerator:
     # has selection of notes and develops a custom chord
     def rand_selection(self) -> None:
         letters = ["A", "B", "C", "D", "E", "F", "G"]
+        accidentals = ["","b","#"]
         numbers = [1, 2, 3, 4, 5, 6, 7]
 
-        basic_notes, notes, chord = self.note_combiner(letters, numbers)
+        basic_notes, notes, chord = self.note_combiner(letters, numbers, accidentals)
         
-        if '-develop-chord' in self.args or '-no-develop-chord' not in self.args:
-            self.audio_note_combiner(notes)   
-        
+        if "-debug-mode" in self.args:
+            print('basic_notes:', basic_notes)
+            print('notes:', notes)
+            print('chord:', chord)
+
+        if "-develop-chord" in self.args or "-no-develop-chord" not in self.args:
+            self.audio_note_combiner(notes)
 
     # chooses notes, and provides namesake of chord
-    def note_combiner(self, letters, numbers) -> tuple():
+    def note_combiner(self, letters, numbers, accidentals) -> tuple():
         basic_notes = []
         notes = []
         no_of_notes = random.randrange(2, 6)
@@ -39,7 +48,7 @@ class ChordGenerator:
             if len(notes) == 0:
                 return number
 
-            comparer = int(notes[0][1])
+            comparer = int(notes[0][-1])
             if abs(comparer - number) > 2:
                 return number_choice(number_list)
 
@@ -47,27 +56,54 @@ class ChordGenerator:
 
         def sort_chord(item):
             item = item[1]
-            return (item[1], item[0])
+            if len(item) == 2:
+                return (item[1], item[0])
+
+            if len(item) == 3:
+                return (item[2], item[0], item[1])
+
+            else:
+                raise ValueError("The note is not the correct size")
 
         for i in range(no_of_notes):
             letter = random.choice(letters)
+            accidental = random.choice(accidentals)
             number = number_choice(numbers)
-            chord_prefix = letter + str(number)
+            if letter == 'B' and accidental == '#':
+                letter = 'C'
+                accidental = ''
+            
+            if letter == 'E' and accidental == '#':
+                letter = 'F'
+                accidental = ''
+                
+            if letter == 'F' and accidental == 'b':
+                letter = 'E'
+                accidental = ''
+            
+            if letter == 'C' and accidental == 'b':
+                letter = 'B'
+                accidental = ''
+            
+            chord_prefix = letter + accidental + str(number)
 
             if chord_prefix not in notes:
-                basic_notes.append(letter)
+                basic_notes.append(letter + accidental)
                 notes.append(chord_prefix)
 
         # combine here using zip, then sort ascending by octave then by note
         zipped = zip(basic_notes, notes)
         zipped = sorted(zipped, key=sort_chord)
+        print(zipped)
         basic_notes = [note[0] for note in zipped]
         notes = [note[1] for note in zipped]
+        print(basic_notes)
+        # problem here that E#, B# and Fb don't exist
         chord = find_chords_from_notes(basic_notes)
 
         if len(chord) == 0:
             try:
-                return self.note_combiner(letters, numbers)
+                return self.note_combiner(letters, numbers, accidentals)
 
             except RecursionError:
                 print("Error happened")
@@ -105,15 +141,18 @@ class ChordGenerator:
 
         sep = "_"
         name = sep.join(name)
-        if any(fnmatch.fnmatch(my_args := argument, 'dest=*') for argument in self.args):
+        
+        if any(
+            fnmatch.fnmatch(my_args := argument, "dest=*") for argument in self.args
+        ):
             store_directory = my_args[5:]
         else:
-            store_directory = 'stored-chords'
-        
+            store_directory = "stored-chords"
+
         try:
             combined_sounds.export(f"{store_directory}/{name}.mp3", format="mp3")
-            
-        except(FileNotFoundError):
+
+        except FileNotFoundError:
             raise FileNotFoundError("Make sure directory exists")
 
 
